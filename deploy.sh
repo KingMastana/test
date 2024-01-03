9b41967ed37ec17da0bda87132c0b01df5e653ab
Joseph Ekpaha-Kwofie <40878648+KingMastana@users.noreply.github.com>
2024-01-03 04:35:07 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 GOOGLE_PROJECT_ID=promptopia-409618
 
-gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/barkbarkapi \
+gcloud builds submit --tag gcr.io/$GOOGLE_PROJECT_ID/mastanaapi \
   --project=$GOOGLE_PROJECT_ID
 
-gcloud run deploy barkbark-api \
+gcloud run deploy mastana-api \
   --image gcr.io/$GOOGLE_PROJECT_ID/mastanaapi \
   --platform managed \
   --region us-central1 \
